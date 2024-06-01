3016b1e0b420a3b0e5f77c0c631a9408747ed738
Saket Karn <saketkarn38@gmail.com>
2024-06-01 11:46:55 +0530

    HOC

--- src/HigherOrderComponent.js
+++ src/HigherOrderComponent.js
@@ -1,28 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+
+// Higher-Order Component
+const withRedBackground = (WrappedComponent) => {
+  return (props) => {
+    return (
+      <div style={{ backgroundColor: "red", textAlign: "center" }}>
+        <WrappedComponent {...props} />
+      </div>
+    );
+  };
+};
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        Click me!
+      </button>
+    </div>
+  );
+};
+
+const CounterWithRedBackground = withRedBackground(Counter);
 
 const HigherOrderComponent = () => {
   return (
     <div>
-      <HOCRed cmp={Counter}/>
+      <CounterWithRedBackground />
     </div>
-  )
-}
+  );
+};
 
-function HOCRed(props){
-    return(
-        <h1 style={{backgroundColor:"red", textAlign:"center"}}><props.cmp/></h1>
-    )
-}
-function Counter(){
-    const [count, setCount] = useState(0)
-    return(
-        <div>
-            <h1>{count}</h1>
-            <button onClick={()=>{
-                setCount(count+1)
-            }}>Click me!</button>
-        </div>
-    )
-}
-
-export default HigherOrderComponent
+export default HigherOrderComponent;
