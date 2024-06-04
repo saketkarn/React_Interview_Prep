a87ba39b23ec1361ba0dbb95e3b1d1480d72e462
Saket Karn <saketkarn38@gmail.com>
2024-06-04 23:20:04 +0530

    t

--- src/Temp.js
+++ src/Temp.js
@@ -1,23 +1,3 @@
-// import React, { useEffect } from 'react'
-
-// const Temp = () => {
-//     useEffect(()=>{
-//         const myInterval=setInterval(()=>{
-//             console.log("hello from setTimeout")
-//         },1000)
-
-//         return ()=> clearInterval(myInterval)
-//     },[])
-//   return (
-//     <div>
-//       <h1>Checking set timeout</h1>
-//     </div>
-//   )
-// }
-
-// export default Temp
-
-
 import React, { useEffect } from 'react';
 
 const Temp = () => {
